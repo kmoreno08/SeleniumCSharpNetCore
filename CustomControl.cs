4a2baa32f0c53347f698ef2cda18ff34c275e881
Kevin Moreno <moren162@mail.chapman.edu>
2021-07-19 17:01:19 -0700

    ComboBoxUserInputMatch added

---
+++ CustomControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace aspnetawesome
+{
+    public class CustomControl : DriverHelper
+    {
+
+        // Checks if user input stays the same after click from dropdown
+        public static void ComboBox(string controlName, string value)
+        {
+            Console.WriteLine("Entered ComboBox...");
+            IWebElement comboControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            // Clear text box
+            comboControl.Clear();
+            // Type in value
+            comboControl.SendKeys(value);
+            //Click drop down menu of the text value
+            Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+
+            // Check if user input value is equal to value in text box
+            bool comboControlExactString = comboControl.GetAttribute("value").Equals(value);
+            // Check if passed/fail
+            if (comboControlExactString)
+            {
+                Console.WriteLine($"Passed with {value}");
+                
+            } else
+            {
+                Console.WriteLine($"Failed with {value}");
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace aspnetawesome
+{
+    public class Tests : DriverHelper
+    {
+       
+        [SetUp]
+        public void Setup()
+        {
+           
+            Console.WriteLine("Setup...");
+            // FireFox Browser
+            Driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void ComboBoxUserInputMatch()
+        {
+            // Go to URL
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            // Maximize Window
+            Driver.Manage().Window.Maximize();
+          
+            // From comboBox, check all values if typed equals the value after click from dropdown
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almonds");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Mango");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Papays");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Banana");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Cherry");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Tomato");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Potato");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Cucumber");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Onion");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Carrot");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Celery");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Broccoli");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Artichoke");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Cauliflower");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Lettuce");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Hazelnuts");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Chestnuts");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Walnuts");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Mongongo");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Rice");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Wheat");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Buckwheat");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Oats");
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Barley");
+        }
+    }
+
+
+    
+}
